0a1a9421da8f41a47fd90511f61883384f94ad0d
wang qun <wang.qun3@zte.com.cn>
2017-12-12 21:30:50 +0800

    init version

---
+++ build.bat
@@ -0,0 +1,15 @@
+set GOPATH=D:\coding\ztesoft\golang\goep;D:\coding\ztesoft\blockchain\ethereum\geth\go_get_github
+set GO15VENDOREXPERIMENT=1
+rem set GOBIN=D:\coding\ztesoft\golang\goep\bin
+
+SET CGO_ENABLED=0
+SET GOOS=windows
+rem SET GOOS=windows linux android darwin
+SET GOARCH=amd64
+rem SET GOARCH=arm 386
+
+go install -x walking/cmd/goep
+go install -x walking/cmd/cdr_tapin
+
+rem go build -o bin/gofast.exe ./src/walking/cmd/gofast
+rem go test walking/svr -v -run="Json"
